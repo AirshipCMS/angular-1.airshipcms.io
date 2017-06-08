86c6751d4cffec38b2c41328db4370f92479fde2
Te Vallee <astronomemusic@gmail.com>
2017-06-07 14:07:23 -1000

    added postcss

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var postcss = require('gulp-postcss');
+var gulp = require('gulp');
+
+gulp.task('css', function () {
+  return gulp.src('./src/*.css')
+    .pipe(postcss())
+    .pipe(gulp.dest('./compartments/assets/styles'));
+});
+
+gulp.task('default', ['css']);
