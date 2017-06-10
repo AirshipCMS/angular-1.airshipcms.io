dcb46e8c973c562b38f7a4d18e044b861167a69a
Te Vallee <astronomemusic@gmail.com>
2017-06-10 10:58:12 -1000

    secure api calls

--- compartments/assets/scripts/app.js
+++ compartments/assets/scripts/app.js
@@ -31,7 +31,7 @@
         })
     })
     .controller('SetupController', ['$scope', '$http', '$sce', function($scope, $http, $sce) {
-      $http.get('http://angular-1.airshipcms.io/api/pages/__root__')
+      $http.get('https://angular-1.airshipcms.io/api/pages/__root__')
         .then(function(res) {
           $scope.title = res.data.name;
           res.data.fields.forEach(function(field) {
@@ -47,7 +47,7 @@
         })
     }])
     .controller('ElementsController', ['$scope', '$http', function($scope, $http) {
-      $http.get('http://angular-1.airshipcms.io/api/aerostat_collection/elements?limit=20&sort=sorting_position')
+      $http.get('https://angular-1.airshipcms.io/api/aerostat_collection/elements?limit=20&sort=sorting_position')
         .then(function(res) {
           $scope.elements = res.data.map(function(element) {
             element.fields.forEach(function(field) {
@@ -62,7 +62,7 @@
         })
     }])
     .controller('ElementController', ['$scope', '$http', '$route', function($scope, $http, $route) {
-      $http.get('http://angular-1.airshipcms.io/api/aerostats/' + $route.current.params.id)
+      $http.get('https://angular-1.airshipcms.io/api/aerostats/' + $route.current.params.id)
         .then(function(res) {
           $scope.element = res.data;
           $scope.element.fields.forEach(function(field) {
@@ -77,7 +77,7 @@
         });
     }])
     .controller('StylingController', ['$scope', '$http', '$sce', function($scope, $http, $sce) {
-      $http.get('http://angular-1.airshipcms.io/api/pages/styling')
+      $http.get('https://angular-1.airshipcms.io/api/pages/styling')
         .then(function(res) {
           res.data.fields.forEach(function(field) {
             switch(field.variable_name) {
@@ -94,7 +94,7 @@
         });
     }])
     .controller('AirshipSchemaController', ['$scope', '$http', '$sce', function($scope, $http, $sce) {
-      $http.get('http://angular-1.airshipcms.io/api/pages/airship-schema')
+      $http.get('https://angular-1.airshipcms.io/api/pages/airship-schema')
         .then(function(res) {
           res.data.fields.forEach(function(field) {
             switch(field.variable_name) {
@@ -111,7 +111,7 @@
         });
     }])
     .controller('TutorialController', ['$scope', '$http', '$sce', function($scope, $http, $sce) {
-      $http.get('http://angular-1.airshipcms.io/api/pages/angular-tutorial')
+      $http.get('https://angular-1.airshipcms.io/api/pages/angular-tutorial')
         .then(function(res) {
           res.data.fields.forEach(function(field) {
             switch(field.variable_name) {
